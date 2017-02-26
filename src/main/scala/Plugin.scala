962502b203d97410cc731c325d1df94ec3a80d20
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2017-02-26 19:50:31 +0900

    version up

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -16,7 +16,8 @@ class Plugin extends gitbucket.core.plugin.Plugin {
     new Version("3.12"),
     new Version("4.0.0"),
     new Version("4.5.0"),
-    new Version("4.9.0"))
+    new Version("4.9.0"),
+    new Version("4.10.0"))
 
   override val controllers = Seq(
     "/*" -> new CommitGraphsController
